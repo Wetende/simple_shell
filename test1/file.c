8e05bed8a0f7e4a07e24f67c148d7da40e4b51db
Favour Michael <michaelfavour418@gmail.com>
2023-05-15 11:36:58 -0700

    file

--- test1/file.c
+++ test1/file.c
@@ -11,130 +11,140 @@
 
 #define BUFSIZE 1024
 #define PROMPT "$ "
-
+/**
+ * get_command - function that gets commands from stdin and generates output
+ * @cmd: commands to get
+ */
 void get_command(char **cmd)
 {
-    write(STDOUT_FILENO, PROMPT, sizeof(PROMPT) - 1);
-    fflush(stdout);
+	write(STDOUT_FILENO, PROMPT, sizeof(PROMPT) - 1);
+	fflush(stdout);
+	*cmd = NULL;
+	size_t bufsize = 0;
 
-    *cmd = NULL;
-    size_t bufsize = 0;
-    if (getline(cmd, &bufsize, stdin) == -1)
-    {
-        if (feof(stdin))
-        {
-            write(STDOUT_FILENO, "\n", 1);
-            exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            perror("getline");
-            exit(EXIT_FAILURE);
-        }
-    }
+	if (getline(cmd, &bufsize, stdin) == -1)
+	{
+		if (feof(stdin))
+		{
+			write(STDOUT_FILENO, "\n", 1);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("getline");
+			exit(EXIT_FAILURE);
+		}
+	}
 }
 
+/**
+ * write_status_message - function that checks for the status of input and
+ * writes a status message accordingly
+ * @message: the message to be printed
+ * @status: the values to be displayed
+ */
 void write_status_message(const char *message, int status)
 {
-    char buffer[BUFSIZE];
-    int len = 0;
-    int temp = status;
+	char buffer[BUFSIZE];
+	int len = 0;
+	int temp = status;
 
-    if (temp < 0)
-    {
-        buffer[len++] = '-';
-        temp = -temp;
-    }
-    else if (temp == 0)
-    {
-        buffer[len++] = '0';
-    }
+	if (temp < 0)
+	{
+		buffer[len++] = '-';
+		temp = -temp;
+	}
+	else if (temp == 0)
+	{
+		buffer[len++] = '0';
+	}
+	while (temp != 0)
+	{
+		buffer[len++] = '0' + (temp % 10);
+		temp /= 10;
+	}
+	if (status < 0)
+	{
+		buffer[len++] = '-';
+	}
+	for (int i = 0; i < len / 2; i++)
+	{
+		char tmp = buffer[i];
 
-    while (temp != 0)
-    {
-        buffer[len++] = '0' + (temp % 10);
-        temp /= 10;
-    }
+		buffer[i] = buffer[len - i - 1];
+		buffer[len - i - 1] = tmp;
+	}
 
-    if (status < 0)
-    {
-        buffer[len++] = '-';
-    }
+	buffer[len] = '\n';
 
-    for (int i = 0; i < len / 2; i++)
-    {
-        char tmp = buffer[i];
-        buffer[i] = buffer[len - i - 1];
-        buffer[len - i - 1] = tmp;
-    }
-
-    buffer[len] = '\n';
-
-    write(STDOUT_FILENO, message, strlen(message));
-    write(STDOUT_FILENO, buffer, len + 1);
+	write(STDOUT_FILENO, message, strlen(message));
+	write(STDOUT_FILENO, buffer, len + 1);
 }
 
+/**
+ * execute_command - function that takes in commands from stdin and
+ * executes them.
+ * @cmd: commands to take in
+ */
 void execute_command(char *cmd)
 {
-    char *args[BUFSIZE]; // Array of arguments
-    int argc = 0;       // Number of arguments
-    char *token;
+	char *args[BUFSIZE];
+	int argc = 0;
+	char *token;
 
-    // Tokenize the command into arguments
-    token = strtok(cmd, " \t\n");
-    while (token != NULL)
-    {
-        args[argc++] = token;
-        token = strtok(NULL, " \t\n");
-    }
-    args[argc] = NULL; // Set the last element of args to NULL
+	token = strtok(cmd, " \t\n");
+	while (token != NULL)
+	{
+		args[argc++] = token;
+		token = strtok(NULL, " \t\n");
+	}
+	args[argc] = NULL;
+	if (strcmp(args[0], "exit") == 0)
+	{
+		exit(0);
+	}
+	pid_t pid;
+	int status;
 
-    if (strcmp(args[0], "exit") == 0)
-    {
-        exit(0); // Exit the shell program
-    }
-
-    pid_t pid;
-    int status;
-
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        execvp(args[0], args);
-        perror(args[0]);
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-        if (WIFEXITED(status))
-        {
-            write_status_message("Child exited with status ", WEXITSTATUS(status));
-        }
-        else if (WIFSIGNALED(status))
-        {
-            write_status_message("Child killed by signal ", WTERMSIG(status));
-        }
-    }
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	} else if (pid == 0)
+	{
+		execvp(args[0], args);
+		perror(args[0]);
+		exit(EXIT_FAILURE);
+	} else
+	{
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+		{
+			write_status_message("Child exited with status ", WEXITSTATUS(status));
+		}
+		else if (WIFSIGNALED(status))
+		{
+			write_status_message("Child killed by signal ", WTERMSIG(status));
+		}
+	}
 }
 
+/**
+ * main - Entry point
+ * Return: 0 on success
+ */
 int main(void)
 {
-    char *cmd = NULL;
+	char *cmd = NULL;
 
-    while (1)
-    {
-        get_command(&cmd);
-        execute_command(cmd);
-    }
+	while (1)
+	{
+		get_command(&cmd);
+		execute_command(cmd);
+	}
 
-    free(cmd);
+	free(cmd);
 
-    return 0;
+	return (0);
 }
-
