c3a86f162a96f1f27b8b32d8fce405fa9011e707
Wetende <cyprianwetende@gmail.com>
2023-05-20 22:36:35 -0700

    environ

--- environ.c
+++ environ.c
@@ -8,22 +8,22 @@
  */
 int main(void)
 {
-    extern char **environ;
-    int i = 0;
+	extern char **environ;
+	int i = 0;
 
-    while (environ[i] != NULL)
-    {
-        char *env_var = environ[i];
-        size_t len = 0;
-        while (env_var[len] != '\0')
-            len++;
+	while (environ[i] != NULL)
+	{
+		char *env_var = environ[i];
+		size_t len = 0;
+		while (env_var[len] != '\0')
+			len++;
 
-        write(STDOUT_FILENO, env_var, len);
-        write(STDOUT_FILENO, "\n", 1);
+		write(STDOUT_FILENO, env_var, len);
+		write(STDOUT_FILENO, "\n", 1);
 
-        i++;
-    }
+		i++;
+	}
 
-    return 0;
+	return (0);
 }
 
